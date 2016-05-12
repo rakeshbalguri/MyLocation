3dd1dc1ba24ec70c40c36f6209bdbcccdc927918
Rakesh Balguri <sagra02@ca.com>
2016-05-12 14:37:17 +0530

    Initial commit

---
+++ app/src/main/java/com/rakesh/mylocation/MapsActivity.java
@@ -0,0 +1,111 @@
+package com.rakesh.mylocation;
+
+import android.location.Location;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements LocationProvider.LocationCallback {
+
+    public static final String TAG = MapsActivity.class.getSimpleName();
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private LocationProvider mLocationProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        System.out.println("########### onCreate Setting up map");
+
+        setUpMapIfNeeded();
+
+        System.out.println("########### onCreate Map Setup Success");
+
+        mLocationProvider = new LocationProvider(this, this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("########### onResume");
+        setUpMapIfNeeded();
+        System.out.println("########### onResume Map Setup Success");
+        mLocationProvider.connect();
+        System.out.println("########### onResume Connection Success");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("########### onPause");
+        mLocationProvider.disconnect();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            System.out.println("########### MapObject Created "+mMap);
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    public void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+        System.out.println("########### handleNewLocation handleNewLocation");
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        System.out.println("########### handleNewLocation Current Location:"+latLng);
+
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!");
+        mMap.addMarker(options);
+        System.out.println("########### handleNewLocation added the marker:");
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+}
